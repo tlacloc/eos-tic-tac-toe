70cafd8e954dc15ab0b62fc7575b594c6475588d
Erick Casanova <desneruda@pop-os.localdomain>
2021-09-16 14:01:53 -0500

    Tic tac toe contract v1

---
+++ src/tictactoe.cpp
@@ -0,0 +1,16 @@
+#include <eosio/eosio.hpp>
+
+using namespace eosio;
+
+CONTRACT tictactoe : public contract {
+
+  public:
+
+    using contract::contract;
+
+    ACTION welcome(name host, name challenger) {
+
+      require_auth( get_self() );
+      print( "Welcome, challengers ", host.value, " and ",challenger.value );
+    }
+};
